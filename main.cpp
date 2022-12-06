ff49b4a8695338f417d374ed3875780116030518
Jakub Nowak <jakubnowak712@gmail.com>
2022-12-07 00:32:52 +0100

    nie pamietam co dodalem w tym commicie

--- main.cpp
+++ main.cpp
@@ -25,53 +25,73 @@ int main(void) {
     using namespace std;
 
     auto screen = ScreenInteractive::Fullscreen();
+    int wybrane_okno = 0;
 
-    vector<string> entries = {
-        "zupa",
-        "eeeee",
-        "dźwig",
-    };
-    int selected = 0;
-    auto menu = Menu(&entries, &selected);
-
-    auto exit = Button("Wyjdź",  screen.ExitLoopClosure(), ButtonOption::Ascii());
 
     string imie = "";
-    auto input = Input(&imie, "Podaj imię");
+    auto podaj_imie = Input(&imie, "");
 
-    string xd = "";
-    auto xdd = Input(&xd, "ddd");
+    string nazwisko = "";
+    auto podaj_nazwisko = Input(&nazwisko, "nazwisko");
 
-    auto component = Container::Vertical({
-        input | flex | borderRounded,
-        xdd,
-        exit | align_right,
-        menu,
+    auto guzik_karta_dan = Button("Przejdź do karty dań", [&] {
+        if (imie == "") return;
+        wybrane_okno = 1;
     });
 
-    auto component_renderer = Renderer(component, [&] {
-        return window(exit->Render() | align_right, vbox({
-            vbox({
-                text("KARCZMA POD SUPERNOWĄ") | center,
-                text("siema " + imie),
-                text("wybrałeś pozycję " + entries[selected] + " (" + std::to_string(selected) + ")")
-                
-            }) | borderRounded | size(WIDTH, LESS_THAN, 90) | size(WIDTH, GREATER_THAN, 61) | center,
-            hbox({
-                window(text("Formularz") | hcenter, vbox({
-                    window(text("Twoje imię"), input->Render()),
-                    xdd->Render()
-
-                })) | size(WIDTH, EQUAL, 31),
-                window(text("Menu") | hcenter, menu->Render())
-            }) | center | flex,
-            hbox({
-                text(" Dzisiaj mamy: [data]"),
-            })
-        }));
+    auto guzik_koszyk = Button("Przejdź do koszyka", [&] {
+        wybrane_okno = 2;
     });
 
-    screen.Loop(component_renderer);
+    auto okno_witaj = Renderer([&] {
+        return vbox(
+            text("Witaj w Karczmie pod Supernową") | center | size(HEIGHT, EQUAL, 3),
+            paragraphAlignCenter("srutututu jakis opis Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.") | size(WIDTH, LESS_THAN, 60) | center,
+            text(""),
+            window(text("Twoje imie:"), podaj_imie->Render())
+            | size(WIDTH, EQUAL, 40)
+            | center,
+            text(""),
+            guzik_karta_dan->Render() | center
+        );
+    });
+
+
+    auto okno_karta_dan = Renderer([&] {
+        return vbox({
+           text("karta dań"),
+           guzik_koszyk->Render()
+        });
+    });
+
+    auto okno_koszyk = Renderer([&] {
+        return vbox({
+        });
+    });
+
+    auto okno = Container::Tab({
+       okno_witaj,
+       okno_karta_dan,
+       okno_koszyk
+    }, &wybrane_okno);
+
+    auto exit = Button("Wyjdź",  screen.ExitLoopClosure(), ButtonOption::Ascii());
+    auto komponenty = Container::Vertical({
+        okno,
+        exit,
+        podaj_imie,
+        guzik_karta_dan,
+        podaj_nazwisko,
+        guzik_koszyk,
+    });
+
+    auto renderer = Renderer(komponenty, [&] {
+        return window(exit->Render() | align_right,
+            okno->Render()
+        );
+    });
+
+    screen.Loop(renderer);
 
     return 0;
 }
