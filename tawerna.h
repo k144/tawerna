922c4f421c383923d5b4c5b38ca5767c012edb35
michalkry4 <95204187+MichuKry@users.noreply.github.com>
2022-12-09 11:22:54 +0100

    Zmienilem twoja koncepcje string paragon na cos innego

--- tawerna.h
+++ tawerna.h
@@ -58,7 +58,39 @@ public:
     vector<Pozycja> pozycje {};
     int zliczKoszt();
 };
+//Zrobiłem paragon na voidzie z tego wzgledu ze nie jest wymagany zrobienia go w stringu
+//i tak jest najprosciej (wedlug mnie)
+void paragon()
+{
+    Zamowienie zamowienie;
+    fstream plik;
+    plik.open("paragon.txt", ios::in | ios::out);
+    if (plik.is_open()) {
+        plik << zamowienie.numer;
+        plik << zamowienie.imieKlienta;
+        plik << zamowienie.adres;
+        plik << zamowienie.naWynos;
+        plik << zamowienie.stolik;
+        plik << zamowienie.preferowanyCzas;
+        plik << zamowienie.zliczKoszt();
+    }
+    plik.close();
 
+}
+//tu zrobilem void do wyswietlenie paragonu. Mozna zostawic tak jak jest ze samo paragon()
+//or zrobic tak jak ania ze kazda zmienna osobno sie wyswietla. To zostawiam Tobie do wybrania
+void pokazParagon()
+{
+    Zamowienie zamowienie;
+    fstream plik;
+    plik.open("paragon.txt", ios::in);
+    if (plik.is_open())
+    {
+        paragon();
+    }
+    plik.close();
+
+}
 void wypisz(const Danie &danie, string end);
 void wypisz(const Zamowienie &zamowienie);
 void wypisz(const Pozycja &pozycja, string end);
